e7f598e3147fe17b72aed583296f8bab2d8390cd
prashantkumar342 <tuhaniaji@gmail.com>
2026-01-20 16:18:53 +0530

    server cors update

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,32 @@
 import http from 'http';
-import app from './app';
+import cors from 'cors';
 import { connectDB } from './config/db';
 import { Server } from 'socket.io';
 import { setupSocket } from './socket';
 import { ENV } from './config/env';
+import cookieParser from 'cookie-parser';
+import express from 'express';
+import router from './routes';
 
 export const startServer = async (): Promise<void> => {
   try {
     await connectDB();
+
+    const app = express();
+
+    app.use(cookieParser());
+    app.use(
+      cors({
+        credentials: true,
+        origin: process.env.APP_URL,
+        methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+        allowedHeaders: ['Content-Type', 'Authorization', 'Cookie'],
+      }),
+    );
+    app.use(express.json());
+
+    // apis
+    app.use('/api/teammanager', router);
 
     const server = http.createServer(app);
     // initSocket server;
